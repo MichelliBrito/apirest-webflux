2a5f8d456d96f406d5595253caed3f9e4dfe35ef
michellibrito <michellidibrito@gmail.com>
2019-10-28 10:21:07 -0300

    inserindo exemplo de flux

--- src/main/java/com/apirest/webflux/WebfluxApplication.java
+++ src/main/java/com/apirest/webflux/WebfluxApplication.java
@@ -3,7 +3,7 @@ package com.apirest.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import reactor.core.publisher.Flux;
 
 
 @SpringBootApplication
@@ -11,7 +11,15 @@ public class WebfluxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebfluxApplication.class, args);
-		System.out.print("Ola mundo com webflux!!! ");
+		System.out.println("Ola mundo com webflux!!! ");
+
+		Flux<String> colorFlux = Flux.just("black", "white", "blue");
+
+			colorFlux
+				.log()
+				.map(String::toUpperCase);
+//			.subscribe();
+//			.subscribe(System.out::println);
 	}
 	
 
